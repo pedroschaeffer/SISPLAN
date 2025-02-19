11033b78b996144086d8206d8ed0070c891fd298
Pedro Vinicius Perez Schaeffer <pedro.perez@sisplan.com.br>
2025-02-19 15:01:44 -0300

    preenchimento dinamico adicionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AutocompleteComponent } from './autocomplete/autocomplete.component';
 import { CardGridComponent } from "./card-grid/card-grid.component";
+import { PreenchimentoDinamicoComponent } from './preenchimento-dinamico/preenchimento-dinamico.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AutocompleteComponent, CardGridComponent],
+  imports: [RouterOutlet, AutocompleteComponent, CardGridComponent, PreenchimentoDinamicoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/preenchimento-dinamico/preenchimento-dinamico.component.ts
+++ src/app/preenchimento-dinamico/preenchimento-dinamico.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'
+
+
+interface Convidado {
+  nome: string;
+  contato: string;
+}
 
 @Component({
   selector: 'app-preenchimento-dinamico',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './preenchimento-dinamico.component.html',
   styleUrl: './preenchimento-dinamico.component.css'
 })
 export class PreenchimentoDinamicoComponent {
 
+  convidados: Convidado[] = [];
+
+  adicionarConvidado() {
+    const novoConvidado: Convidado = { nome: '', contato: '' };
+    this.convidados = [...this.convidados, novoConvidado]; // Cria um novo array
+  }
+
+
+  // Caso preciso do boão remover
+  // removerConvidado(index: number) {
+  //   this.convidados.splice(index, 1);
+  // }
 }
